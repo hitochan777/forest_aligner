1c2e68a196e0b647f910fc7bfad15d6fccdec94f
Otsuki Hitoshi <hitochan777@gmail.com>
2015-10-24 23:22:48 +0900

    Modified train.sh

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=/home/chu/tools/boost_1_54_0/lib:$PYTHONPATH
 export LD_LIBRARY_PATH=/home/chu/tools/boost_1_54_0/lib:$LD_LIBRARY_PATH
 NUMCPUS=$CORES
 
-APP_DIR=$HOME/developer/forest_aligner
+APP_DIR=.
 K=128
 LINK=$1
 MAXEPOCH=$2
