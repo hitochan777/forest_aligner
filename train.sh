939e73dde50e411eeafd682e08df8ddb0b3a92f4
Otsuki Hitoshi <hitochan777@gmail.com>
2015-10-24 23:57:24 +0900

    Modified train.sh

--- train.sh
+++ train.sh
@@ -40,7 +40,32 @@ nice -15 mpiexec -n $NUMCPUS $PYTHON $APP_DIR/aligner.py \
   --train \
   --k $K 1> $NAME.out 2> $NAME.err
 
-cat $NAME.err
+nice -15 mpiexec -n $NUMCPUS $PYTHON $APP_DIR/aligner.py \
+  --f $DATA/train.f \
+  --e $DATA/train.e \
+  --gold $DATA/train.a.s \
+  --ftrees $DATA/forest/train.f.forest \
+  --etrees $DATA/forest/1best/train.e.forest \
+  --fdev $DATA/dev.f \
+  --edev $DATA/dev.e \
+  --ftreesdev $DATA/forest/dev.f.forest \
+  --etreesdev $DATA/forest/1best/dev.e.forest \
+  --golddev $DATA/dev.a.s \
+  --evcb $DATA/e.vcb \
+  --fvcb $DATA/f.vcb \
+  --pef $DATA/GIZA++.m4.pef \
+  --pfe $DATA/GIZA++.m4.pfe \
+  --a1 $DATA/train.m4gdfa.e-f \
+  --a2 $DATA/train.nakazawa.e-f.s \
+  --a1_dev $DATA/dev.m4gdfa.e-f \
+  --a2_dev $DATA/dev.nakazawa.e-f.s \
+  --langpair $LANGPAIR \
+  --partial $PARTIAL \
+  --maxepochs $MAXEPOCH \
+  --nto1 $LINK \
+  --train \
+  --k $K 1> $NAME.out 2> | tee $NAME.err 
+
 cat $NAME.out
 
 ITER=`grep F-score-dev $NAME.err | awk '{print $2}' | cat -n | sort -nr -k 2 | head -1 | cut -f 1`
