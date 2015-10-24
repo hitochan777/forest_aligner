84a8b9180a5e32e0177445008480c3022c84c716
Otsuki Hitoshi <hitochan777@gmail.com>
2015-10-24 23:21:23 +0900

    Modified train.sh

--- train.sh
+++ train.sh
@@ -40,6 +40,9 @@ nice -15 mpiexec -n $NUMCPUS $PYTHON $APP_DIR/aligner.py \
   --train \
   --k $K 1> $NAME.out 2> $NAME.err
 
+cat $NAME.err
+cat $NAME.out
+
 ITER=`grep F-score-dev $NAME.err | awk '{print $2}' | cat -n | sort -nr -k 2 | head -1 | cut -f 1`
 WEIGHTS_FILE=weights.`head -1 $NAME.out`
 ./extract-weights.py $WEIGHTS_FILE $ITER $NAME
