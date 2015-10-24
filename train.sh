5e6ff574e2627060c2c1c28b1b58f1accbdb3710
Hitoshi Otsuki <ilikestars55@yahoo.co.jp>
2015-10-24 21:35:31 +0900

    Test modification for jenkins

--- train.sh
+++ train.sh
@@ -10,7 +10,6 @@ export PATH=/home/chu/mpich-install/bin:$PATH
 export PYTHONPATH=/home/chu/tools/boost_1_54_0/lib:$PYTHONPATH
 export LD_LIBRARY_PATH=/home/chu/tools/boost_1_54_0/lib:$LD_LIBRARY_PATH
 NUMCPUS=$CORES
-# NUMCPUS=`wc -l $PBS_NODEFILE | awk '{print $1}'`
 ###################################################################
 
 APP_DIR=$HOME/developer/forest_aligner
