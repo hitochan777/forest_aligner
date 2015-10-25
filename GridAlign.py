9b95efd72327ef2bba8afa50aebecb6d0a8f4ddd
Otsuki Hitoshi <hitochan777@gmail.com>
2015-10-26 08:45:11 +0900

    With Local SrcTgtTag feature

--- GridAlign.py
+++ GridAlign.py
@@ -185,7 +185,7 @@ class Model(object):
       self.featureTemplates.append(localFeatures.ff_nonfinalPeriodLinkedToComma)
       self.featureTemplates.append(localFeatures.ff_nonPeriodLinkedToPeriod)
       self.featureTemplates.append(localFeatures.ff_nonfinalPeriodLinkedToFinalPeriod)
-      # self.featureTemplates.append(localFeatures.ff_tgtTag_srcTag)
+      self.featureTemplates.append(localFeatures.ff_tgtTag_srcTag)
       self.featureTemplates.append(localFeatures.ff_thirdParty)
       # self.featureTemplates.append(localFeatures.ff_continuousAlignment)
   
@@ -199,7 +199,7 @@ class Model(object):
       # self.featureTemplates_nonlocal.append(nonlocalFeatures.ff_nonlocal_dummy)
       self.featureTemplates_nonlocal.append(nonlocalFeatures.ff_nonlocal_isPuncAndHasMoreThanOneLink)
       self.featureTemplates_nonlocal.append(nonlocalFeatures.ff_nonlocal_sameWordLinks)
-      self.featureTemplates_nonlocal.append(nonlocalFeatures.ff_nonlocal_hyperEdgeScore)
+      # self.featureTemplates_nonlocal.append(nonlocalFeatures.ff_nonlocal_hyperEdgeScore)
       # self.featureTemplates_nonlocal.append(nonlocalFeatures.ff_nonlocal_treeDistance)
       # self.featureTemplates_nonlocal.append(nonlocalFeatures.ff_nonlocal_tgtTag_srcTag)
       # self.featureTemplates_nonlocal.append(nonlocalFeatures.ff_nonlocal_crossb)
