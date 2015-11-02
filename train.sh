5468ffb76175638de82b544d877f86df8b48cfa2
Hitoshi Otsuki <ilikestars55@yahoo.co.jp>
2015-11-02 18:26:01 +0900

    Fix bug

--- train.sh
+++ train.sh
@@ -42,4 +42,4 @@ cat $NAME.out
 
 ITER=`grep F-score-dev $NAME.err | awk '{print $2}' | cat -n | sort -nr -k 2 | head -1 | cut -f 1 | tr -d '[[:space:]]'`
 WEIGHTS_FILE=weights.`head -1 $NAME.out`
-./weight-extract.py $WEIGHTS_FILE $ITER $NAME
+./weight_extract.py $WEIGHTS_FILE $ITER $NAME
